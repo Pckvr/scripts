a916150d58eb3b7835f1246b84c0360904f3f573
JohnnyVM <Jonathan.Peckover@:academytrainee.com>
2019-07-12 09:40:57 +0100

    gcloud-sdk

--- gcp-terraform/setup-gcp-sdk.sh
+++ gcp-terraform/setup-gcp-sdk.sh
@@ -7,24 +7,24 @@ sudo apt-get install -y wget
 sudo apt-get install -y curl
 
 #prequisites python
-#sudo apt-get install build-essential checkinstall
-#sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+sudo apt-get install build-essential checkinstall
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 
-#download python2.7
-#cd /usr/src
-#sudo wget https://www.python.org/ftp/python/2.7.16/Python-2.7.16.tgz
+download python2.7
+cd /usr/src
+sudo wget https://www.python.org/ftp/python/2.7.16/Python-2.7.16.tgz
 
-#extract python2.7
-#sudo tar xzf Python-2.7.16.tgz
+extract python2.7
+sudo tar xzf Python-2.7.16.tgz
 
-#compile python source
-#cd Python-2.7.16
-#sudo ./configure --enable-optimizations
-#sudo make altinstall
+compile python source
+cd Python-2.7.16
+sudo ./configure --enable-optimizations
+sudo make altinstall
 
-#check install python2.7 -V
+check install python2.7 -V
 
-#go to home directory 
+go to home directory 
 cd ~
 
 #download linux64-bit archive file
