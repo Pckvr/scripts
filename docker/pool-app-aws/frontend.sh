5a20ceb03c0b19f5f7e7dadbf9dbc6a071f73352
JohnnyQA <Jonathan.Peckover@academytrainee.com>
2019-07-24 11:08:09 +0100

    aws separate scripts

--- docker/pool-app-aws/frontend.sh
+++ docker/pool-app-aws/frontend.sh
@@ -7,14 +7,7 @@ sudo rm -rf pool-app
 mkdir pool-app
 cd pool-app
 
-git clone https://github.com/JPeckoverQA/TeamAPoolProjectBackend.git
 git clone https://github.com/JPeckoverQA/TeamAPoolProjectUI.git
-cd TeamAPoolProjectBackend
-git checkout docker-aws
-./setup-backend.sh
-
-cd ..
-
 cd TeamAPoolProjectUI
 git checkout docker-multistage
 ./setup-frontend.sh
