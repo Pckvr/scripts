f53e37a841991e26f616bbe408d4fd1830ac0ac2
JohnnyQA <Jonathan.Peckover@academytrainee.com>
2019-07-24 10:09:31 +0100

    backend branch does not include mongo

---
+++ docker/pool-app-multistage-no-mongo/pool-app.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo apt update
+sudo apt install git
+
+cd ~/
+sudo rm -rf pool-app
+mkdir pool-app
+cd pool-app
+
+git clone https://github.com/JPeckoverQA/TeamAPoolProjectBackend.git
+git clone https://github.com/JPeckoverQA/TeamAPoolProjectUI.git
+cd TeamAPoolProjectBackend
+git checkout docker-no-mongo
+./setup-backend.sh
+
+cd ..
+
+cd TeamAPoolProjectUI
+git checkout docker-multistage
+./setup-frontend.sh
