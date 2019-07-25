5e4505c7feae721cc7342fcddfc7fbbcb68eac69
JohnnyQA <Jonathan.Peckover@academytrainee.com>
2019-07-25 10:47:26 +0100

    open port 80

--- aws/terraform/security-group.tf
+++ aws/terraform/security-group.tf
@@ -8,6 +8,12 @@ resource "aws_security_group" "default" {
     protocol = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
+    ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port = 0
     to_port = 0
