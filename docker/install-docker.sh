a42fa6d52cabd774a726e938f5868cdc2425116e
JPeckoverQA <50712067+JPeckoverQA@users.noreply.github.com>
2019-07-25 13:52:45 +0100

    typo

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -6,6 +6,6 @@ curl https://get.docker.com | sudo bash
 
 sudo usermod -aG docker $(whoami)
 
-//sudo newgrp docker
+#sudo newgrp docker
 
-//echo "YOU MUST NOW RESTART THE VM"
+#echo "YOU MUST NOW RESTART THE VM"
