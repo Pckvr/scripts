5d960ad6ffcffe4e93681e5b64a607370910c6fc
JohnnyQA <Jonathan.Peckover@academytrainee.com>
2019-07-26 11:22:49 +0100

    open port 80

--- aws/terraform-3vms/security-group-frontend.tf
+++ aws/terraform-3vms/security-group-frontend.tf
@@ -14,6 +14,12 @@ resource "aws_security_group" "frontend" {
     protocol = "TCP"
     cidr_blocks = ["${aws_subnet.default.cidr_block}"]
   }
+    ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "TCP"
+    cidr_blocks = ["${aws_subnet.default.cidr_block}"]
+  }
   egress {
     from_port = 0
     to_port = 0
