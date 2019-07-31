e641e5cdb2ad34aa9ea43a2da7d7719ee11818dc
root <root@drone.europe-west2-c.c.super-squirtle-squad.internal>
2019-07-31 10:54:46 +0000

    added comments

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -6,6 +6,10 @@ curl https://get.docker.com | sudo bash
 
 sudo usermod -aG docker $(whoami)
 
-#sudo newgrp docker
+sudo newgrp docker
+
+echo "*************************************************************************************"
+echo "**********DOCKER USER HAS BEEN ADDED AND INITIALISED**********"
+echo "*************************************************************************************"
 
 #echo "YOU MUST NOW RESTART THE VM"
