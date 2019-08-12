be2d749ba144da0a041ead94a57ada331c164e45
JPeckoverQA <50712067+JPeckoverQA@users.noreply.github.com>
2019-08-12 10:54:17 +0100

    added warnings and instructions

--- kubernetes/install-kubectl.sh
+++ kubernetes/install-kubectl.sh
@@ -5,3 +5,18 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
+
+echo " "
+echo "********************************************************************************"
+echo " "
+echo "MUST HAVE INSTALLED AND INITIALISED GCP SDK"
+echo " "
+echo "********************************************************************************"
+echo " "
+echo "MUST HAVE A CLUSTER RUNNING"
+echo " "
+echo "********************************************************************************"
+echo " "
+echo "MUST CONNECT THE CLUSTER TO THE VM"
+echo " "
+echo "********************************************************************************"
