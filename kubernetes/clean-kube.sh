16a69d547d1de8f01776dcde2afd97947c4f53bf
Johnny <Jonathan.Peckover@academytrainee.com>
2019-08-13 13:20:10 +0000

    clean kube update

--- kubernetes/clean-kube.sh
+++ kubernetes/clean-kube.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 kubectl delete daemonsets,replicasets,services,deployments,pods,rc,ing,statefulset --all
+
+kubectl delete deployments --all
+kubectl delete --all pods
+kubectl delete services --all
+
+kubectl delete pod --all
+kubectl delete service --all
+kubectl delete pvc --all
+kubectl delete configmap --all
+
+kubectl get all
