df8031cf4ca27c21e3988b0196502d1c632b5c50
JPeckoverQA <50712067+JPeckoverQA@users.noreply.github.com>
2019-08-14 14:02:28 +0100

    Update docker-ce-install.sh

--- docker/docker-ce-install.sh
+++ docker/docker-ce-install.sh
@@ -19,7 +19,7 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
-sudo usermod -aG ${USER} docker
-sudo gpasswd -a ${USER} docker
+sudo usermod -aG docker ${USER} 
+sudo gpasswd -a docker ${USER} 
 newgrp docker
 
